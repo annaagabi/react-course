5fdf198fd9548644a6313adb3e8a467dcf96eead
annaagabi <annaa0478@gmail.com>
2024-08-24 16:21:42 -0300

    Feat: restante das informações

--- src/components/section/index.jsx
+++ src/components/section/index.jsx
@@ -6,6 +6,9 @@ export default function Section(){
             <div className="section-container">
                 <h1>Resonate with the visitor's problem</h1>
                 <p>Describe exactly what your product or service does to solve this problem. Avoid using verbose words or phrases.</p>
+                <input type="email" placeholder="Enter your email" />
+                <button type="button">Try it for free</button>
+                <p>By clicking Sign Up you're confirming that you agree with our Terms and Conditions.</p>
             </div>
             <img src={Placeholder} alt="Imagem com fundo cinza e um ícone de imagem dentro" />
         </section>

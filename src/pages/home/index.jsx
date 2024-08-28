d1f60dfc8836cf6fb59cd1319ed3ae8720568194
annaagabi <annaa0478@gmail.com>
2024-08-28 20:01:02 -0300

    Feat: adicionando seção de cards

--- src/pages/home/index.jsx
+++ src/pages/home/index.jsx
@@ -1,11 +1,13 @@
 import Header from "../../components/header"
 import Section from "../../components/section"
+import CardsBenefit from "../../components/cards-benefit"
 
 export default function Home(){
     return (
         <div>
         <Header />
         <Section />
+        <CardsBenefit />
         </div>
     )
 }

114a90bf1219b1e1c0329db08376a1770aaa8617
annaagabi <annaa0478@gmail.com>
2024-08-27 21:38:06 -0300

    Feat: chamando os componentes da página na home

--- src/pages/home/index.jsx
+++ src/pages/home/index.jsx
@@ -1,7 +1,11 @@
 import Header from "../../components/header"
+import Section from "../../components/section"
 
 export default function Home(){
     return (
+        <div>
         <Header />
+        <Section />
+        </div>
     )
 }

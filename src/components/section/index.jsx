e2f144ae93734a5ceb44337b291dc695b2b1f97d
annaagabi <annaa0478@gmail.com>
2024-08-27 21:39:30 -0300

    Feat: primeira seção finalizada

--- src/components/section/index.jsx
+++ src/components/section/index.jsx
@@ -1,4 +1,5 @@
 import Placeholder from "../../assets/image/Placeholder Image.svg"
+import './style.css'
 
 export default function Section(){
     return(
@@ -6,8 +7,10 @@ export default function Section(){
             <div className="section-container">
                 <h1>Resonate with the visitor's problem</h1>
                 <p>Describe exactly what your product or service does to solve this problem. Avoid using verbose words or phrases.</p>
-                <input type="email" placeholder="Enter your email" />
-                <button type="button">Try it for free</button>
+                <div className="signup">
+                    <input type="email" placeholder="Enter your email" />
+                    <button type="button">Try it for free</button>
+                </div>
                 <p>By clicking Sign Up you're confirming that you agree with our Terms and Conditions.</p>
             </div>
             <img src={Placeholder} alt="Imagem com fundo cinza e um ícone de imagem dentro" />

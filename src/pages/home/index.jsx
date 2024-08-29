498f7927343554b02d0d085d96a46694ed6b933d
annaagabi <annaa0478@gmail.com>
2024-08-29 20:24:25 -0300

    Feat: adicionando seção headline

--- src/pages/home/index.jsx
+++ src/pages/home/index.jsx
@@ -2,12 +2,15 @@ import Header from "../../components/header"
 import Section from "../../components/section"
 import CardsBenefit from "../../components/cards-benefit"
 
+import Headline from "../../components/headline"
+
 export default function Home(){
     return (
         <div>
         <Header />
         <Section />
         <CardsBenefit />
+        <Headline />
         </div>
     )
 }

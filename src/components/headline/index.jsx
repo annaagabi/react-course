952f5e7018f22be6e7e0a3e9f4969f9176fc82c7
annaagabi <annaa0478@gmail.com>
2024-08-29 21:55:38 -0300

    Fix: trocando o nome das classNames e adicionando um nome a section

--- src/components/headline/index.jsx
+++ src/components/headline/index.jsx
@@ -3,13 +3,13 @@ import Cube from "../../assets/image/Cube.svg"
 
 export default function Headline(){
     return(
-        <section>
-            <div className="section-content">
+        <section className="headline-container">
+            <div className="headline-content">
                 <p>How it works</p>
                 <h2>Headline with USP related to how your product or service works</h2>
                 <button type="button">Learn more</button>
             </div>
-            <div className="section-steps">
+            <div className="hedline-steps">
                 <div className="steps">
                     <div className="steps-icons">
                         <img src={Cube} alt="Cubo com arestas na cor preta, uma das faces na cor preta e as outras faces na cor branca" />

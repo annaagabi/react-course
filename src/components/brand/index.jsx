23a5cdb8f49a56ae006f51a90a698d5e34f73a34
annaagabi <annaa0478@gmail.com>
2024-08-31 14:44:06 -0300

    Feat: deixando o componente com o código limpo

--- src/components/brand/index.jsx
+++ src/components/brand/index.jsx
@@ -4,6 +4,28 @@ import Master from '../../assets/image/mastercard.png'
 import Visa from '../../assets/image/visa.png'
 import Alelo from '../../assets/image/alelo.png'
 
+// Código Limpo
+const brandImages ={
+    alelo: Alelo,
+    american: American,
+    master: Master,
+    visa: Visa
+}
+
+export default function Brand({ brand }){
+    const ImageSrc = brandImages[brand] // Vai ercorrer o array de marcas
+    return ImageSrc ? <img src={ImageSrc} alt={`Imagem da bandeira ${brand}`} /> : <h1>Selecione uma bandeira válida</h1>
+
+}
+
+/*
+// Código sujo
+
+import './style.css'
+import American from '../../assets/image/american-express.png'
+import Master from '../../assets/image/mastercard.png'
+import Visa from '../../assets/image/visa.png'
+import Alelo from '../../assets/image/alelo.png'
 
 export default function Brand({brand}){
     switch(brand){
@@ -22,3 +44,4 @@ export default function Brand({brand}){
     //     <img src="" alt="Icones de marcas de cartões" />
     // )
 }
+*/

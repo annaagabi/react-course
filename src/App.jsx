6a8c0415bafd25f8f9c3a6eafdd45ac26d726a36
annaagabi <annaa0478@gmail.com>
2024-08-31 14:21:43 -0300

    Feat: chamando o componente brand

--- src/App.jsx
+++ src/App.jsx
@@ -1,30 +1,48 @@
-import Card from "./components/card"
-import Box from '../src/assets/image/Cube.svg'
+import Brand from './components/brand'
 
 export default function App(){
   return(
-    <div>
-        <Card 
-          image={Box}
-          imgAlt="Imagem de uma caixa"
-          title="Highlight benefit one"
-          description="Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse varius enim in eros elementum tristique. Duis cursus, mi quis viverra ornare, eros dolor interdum nulla."
-          />
-        <Card 
-          image={Box}
-          imgAlt="Imagem de uma caixa"
-          title="Highlight benefit two"
-          description="Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse varius enim in eros elementum tristique. Duis cursus, mi quis viverra ornare, eros dolor interdum nulla."
-        />
-        <Card 
-          image={Box}
-          imgAlt="Imagem de uma caixa"
-          title="Highlight benefit three"
-          description="Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse varius enim in eros elementum tristique. Duis cursus, mi quis viverra ornare, eros dolor interdum nulla."
-        />
-      </div>
+    <div className="app-container">
+      <Brand 
+          brand="visa"
+      />
+    </div>
   )
 }
+
+
+
+// import Card from "./components/card"
+// import Box from '../src/assets/image/Cube.svg'
+
+// export default function App(){
+//   return(
+//     <div>
+//         <Card 
+//           image={Box}
+//           imgAlt="Imagem de uma caixa"
+//           title="Highlight benefit one"
+//           description="Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse varius enim in eros elementum tristique. Duis cursus, mi quis viverra ornare, eros dolor interdum nulla."
+//           />
+//         <Card 
+//           image={Box}
+//           imgAlt="Imagem de uma caixa"
+//           title="Highlight benefit two"
+//           description="Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse varius enim in eros elementum tristique. Duis cursus, mi quis viverra ornare, eros dolor interdum nulla."
+//         />
+//         <Card 
+//           image={Box}
+//           imgAlt="Imagem de uma caixa"
+//           title="Highlight benefit three"
+//           description="Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse varius enim in eros elementum tristique. Duis cursus, mi quis viverra ornare, eros dolor interdum nulla."
+//         />
+//       </div>
+//   )
+// }
+
+
+
+
 // import Home from './pages/home/index'
 
 // export default function App(){
